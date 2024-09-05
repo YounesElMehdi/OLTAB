3390292eabd7f088e5750c18f9e2a365d94ef3b5
YounesMDev <younes.elmehdi@outlook.com>
2024-09-05 05:43:49 +0200

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace OLTAB_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# OLTAB_Manager
